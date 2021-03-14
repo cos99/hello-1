d1fc784f1985f10b6181d6048fca4b2c0128ca3d
cos99 <49200727+cos99@users.noreply.github.com>
2021-03-14 19:16:22 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,12 @@
+```
 #include <stdio.h>
-int main()
-{char c[50];
-scanf("%s",c);
-printf("hello %s, ai fost hackuit. ai de plata 300 de dolari\n", c); 
-return 0;
-}
 
+int main()
+{
+  char c[50];
+  scanf("%s",c);
+  printf("hello %s, ai fost hackuit. ai de plata 300 de dolari\n", c); 
+  return 0;
+}
+```
 
